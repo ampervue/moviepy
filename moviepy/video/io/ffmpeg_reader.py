cdaf829c349a4a85cf74d0166e0b8a3e1105b389
itschrisL <itschrislytle@gmail.com>
2017-09-22 21:20:25 -0700

    Added yadif filter with 1:-1:1 parameters

--- moviepy/video/io/ffmpeg_reader.py
+++ moviepy/video/io/ffmpeg_reader.py
@@ -90,6 +90,7 @@ class FFMPEG_VideoReader:
                ['-loglevel', 'error',
                 '-f', 'image2pipe',
                 '-vf', 'scale=%d:%d' % tuple(self.size),
+                '-vf', 'yadif=1:-1:1',
                 '-sws_flags', self.resize_algo,
                 "-pix_fmt", self.pix_fmt,
                 '-vcodec', 'rawvideo', '-'])
